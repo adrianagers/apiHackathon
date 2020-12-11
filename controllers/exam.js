65c94e6215aeda8290c40ac8f6501beb16b6ef4a
adrianagers <adrianageraldine.19@gmail.com>
2020-12-11 14:34:17 -0500

    conversion a async await

--- controllers/exam.js
+++ controllers/exam.js
@@ -1,98 +1,103 @@
 const examController = {};
 const ExamModel = require('../models/exam')
 
-examController.create = (req, res) => {
-    if (Object.entries(req.body).length == 0) {
-        return res.status(400).send({
-            message: 'Los datos son obligatorios'
-        })
-    }
-    const exam = new ExamModel({
-        tituloExamen: req.body.tituloExamen,
-        descripcionExamen: req.body.descripcionExamen,
-        lenguajeExamen: req.body.lenguajeExamen,
-        linkExamen: req.body.linkExamen,
-        cargo: req.body.cargo,
-        salario: req.body.salario,
-        empresa: req.body.empresa
-    })
-    exam.save()
-        .then((dataExam) => { res.send(dataExam) })
-        .catch((error) => {
-            res.status(500).send({
-                message: error.message
-            })
-        })
-}
-
-examController.update = (req, res) => {
-    if (Object.entries(req.body).length == 0) {
-        return res.status(400).send({
-            message: 'Los datos son obligatorios.'
-        })
-
-    }
-    
-    const exam = {
-        tituloExamen: req.body.tituloExamen,
-        descripcionExamen: req.body.descripcionExamen,
-        lenguajeExamen: req.body.lenguajeExamen,
-        linkExamen: req.body.linkExamen,
-        cargo: req.body.cargo,
-        salario: req.body.salario,
-        empresa: req.body.empresa
-    }
-    
-    ExamModel.findByIdAndUpdate(req.params.id, exam, { new: true })
-        .then(
-            (examUpdate) => {
-                res.send(examUpdate)
-            }
-        ).catch(
-            (error)=> {
-                res.status(500).send({
-                    message: error.message
-                })
-            }
-        )    
-}
-
-examController.getAll =(req, res) => {
-    ExamModel.find()
-    .populate('companies')
-    .exec()
-    .then((exams) => res.send(exams))
-    .catch(
-        (error) => {
-            res.status(500).send({
-                message: error.nessage
+examController.create = async (req, res) => {
+    try {
+        if (Object.entries(req.body).length == 0) {
+            return res.status(400).send({
+                message: 'Los datos son obligatorios'
             })
         }
-    )
+        const exam = new ExamModel({
+            tituloExamen: req.body.tituloExamen,
+            descripcionExamen: req.body.descripcionExamen,
+            lenguajeExamen: req.body.lenguajeExamen,
+            linkExamen: req.body.linkExamen,
+            cargo: req.body.cargo,
+            salario: req.body.salario,
+            empresa: req.body.empresa
+        })
+        await exam.save()
+        res.send(dataExam)
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
 }
-examController.getOne = (req, res) => {
-    ExamModel.findById(req.params.id)
-    .populate('companies')
-    .exec()
-    .then((exam) => {res.send(exam)})
-    .catch(
-        (error) => {
-            res.status(500).send({
-                message: error.message
+
+examController.update = async (req, res) => {
+
+    try {
+        if (Object.entries(req.body).length == 0) {
+            return res.status(400).send({
+                message: 'Los datos son obligatorios.'
             })
+
         }
-    )
+
+        const exam = {
+            tituloExamen: req.body.tituloExamen,
+            descripcionExamen: req.body.descripcionExamen,
+            lenguajeExamen: req.body.lenguajeExamen,
+            linkExamen: req.body.linkExamen,
+            cargo: req.body.cargo,
+            salario: req.body.salario,
+            empresa: req.body.empresa
+        }
+
+       const examUpdate =  await ExamModel.findByIdAndUpdate(req.params.id, exam, {
+            new: true
+        })
+        res.send(examUpdate)
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+
 }
-examController.deleteOne= (req,res) => {
-    ExamModel.findByIdAndRemove(req.params.id)
-    .then((exam)=> {res.send(exam)})
-    .catch(
-        (error) => {
-            res.status(500).send( {
-                message:error.message
-            })
-        }
-    )
+
+examController.getAll = async (req, res) => {
+    try {
+        await ExamModel.find()
+            .populate('companies')
+            .exec()
+        res.send(exams)
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+}
+examController.getOne = async (req, res) => {
+    try {
+       const exam = await ExamModel.findById(req.params.id)
+            .populate('companies')
+            .exec()
+        res.send(exam)
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+}
+examController.deleteOne = async (req, res) => {
+    try {
+        await ExamModel.findByIdAndRemove(req.params.id)
+        res.json({
+            status: 200,
+            message: `El siguiente ID fue eliminado ${res.params.id}`
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        })
+    }
+
+
+
 }
 
 module.exports = examController
--- controllers/recruit.js
+++ controllers/recruit.js
@@ -1,53 +1,53 @@
 const RecruitModel = require('../models/recruit');
 const recruitController = {};
-recruitController.create = (req, res) => {
+recruitController.create = async (req, res) => {
+    try {
+        if (Object.entries(req.body).length == 0) {
+            return res.status(400).send({
+                message: 'Los datos son obligatorios.'
+            })
+        }
 
-    if (Object.entries(req.body).length == 0) {
-        return res.status(400).send({
-            message: 'Los datos son obligatorios.'
+        const recruit = new RecruitModel({
+            notify: req.body.notify,
+            applicantStatus: req.body.applicantStatus,
+            applicantProcess: req.body.applicantProcess
         })
-    }
 
-    const recruit = new RecruitModel({
-        notify: req.body.notify,
-        applicantStatus: req.body.applicantStatus,
-        applicantProcess: req.body.applicantProcess
-    })
-
-    recruit.save().then((dataRecruit) => {
-        res.send(dataRecruit)
-    }).catch((error) => {
+        await recruit.save().then((dataRecruit) => {
+            res.send(dataRecruit)
+        })
+    } catch (error) {
         res.status(500).send({
             message: error.message
 
         })
-    })
+    }
 }
-recruitController.update = (req, res) => {
-    if (Object.entries(req.body).length == 0) {
-        return res.status(400).send({
-            message: 'Los datos son obligatorios.'
+recruitController.update = async (req, res) => {
+    try {
+        if (Object.entries(req.body).length == 0) {
+            return res.status(400).send({
+                message: 'Los datos son obligatorios.'
+            })
+        }
+
+        const recruit = {
+            notify: req.body.notify,
+            applicantStatus: req.body.applicantStatus,
+            applicantProcess: req.body.applicantProcess
+        }
+
+      const recruitUpdate = await RecruitModel.findByIdAndUpdate(req.params.id, recruit)
+        res.send(recruitUpdate)
+
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
         })
     }
 
-    const recruit = {
-        notify: req.body.notify,
-        applicantStatus: req.body.applicantStatus,
-        applicantProcess: req.body.applicantProcess
-    }
-    
-    RecruitModel.findByIdAndUpdate(req.params.id, recruit)
-        .then(
-            (recruitUpdate) => {
-                res.send(recruitUpdate)
-            }
-        ).catch(
-            (error) => {
-                res.status(500).send({
-                    message: error.message
-                })
-            }
-        )
 }
 
 module.exports = recruitController
